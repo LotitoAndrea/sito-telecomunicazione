75337a590dd1e4ecb17e04c808476852ad41048b
LotitoAndrea <lotito.andrea@tiisgrassi.edu.it>
2024-10-25 13:46:29 +0200

    modifica

--- TLC/script.js
+++ TLC/script.js
@@ -13,3 +13,35 @@ function mostra()
         bottone.innerHTML = "Mostra";
     }
 }
+
+function mostra1()
+{
+    let card = document.getElementById("cardNascosta1");
+    let bottone1 = document.getElementById("bottone1");
+    if(card.classList.contains("d-none"))
+    {
+        card.classList.remove("d-none");
+        bottone1.innerHTML = "Nascondi";
+    }
+    else
+    {
+        card.classList.add("d-none");
+        bottone1.innerHTML = "Mostra";
+    }
+}
+
+function mostra2()
+{
+    let card = document.getElementById("cardNascosta2");
+    let bottone2 = document.getElementById("bottone2");
+    if(card.classList.contains("d-none"))
+    {
+        card.classList.remove("d-none");
+        bottone2.innerHTML = "Nascondi";
+    }
+    else
+    {
+        card.classList.add("d-none");
+        bottone2.innerHTML = "Mostra";
+    }
+}
