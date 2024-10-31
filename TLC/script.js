c14168fd5a4bd3c43fc2c59cb64c99371b1b898f
LotitoAndrea <lotito.andrea@tiisgrassi.edu.it>
2024-10-31 10:17:36 +0100

    modifica

--- TLC/script.js
+++ TLC/script.js
@@ -45,3 +45,48 @@ function mostra2()
         bottone2.innerHTML = "Mostra";
     }
 }
+
+function calcolaTensione()
+{
+    const resistenza = parseFloat(document.getElementById("resistenzaB").value);
+    const intensita = parseFloat(document.getElementById("intensitaB").value);
+    
+    const tensione = resistenza * intensita;
+    document.getElementById("risultatoB").innerText = "tensione: " + tensione + " volt";
+
+    if (resistenza === 0 || intensita === 0)
+        {
+            alert("Inserisci dei valori validi.");
+            return;
+        }
+}
+
+function calcolaIntensita()
+{
+    const resistenza = parseFloat(document.getElementById("resistenzaA").value)
+    const tensione = parseFloat(document.getElementById("tensioneA").value)
+
+    const intensita = tensione / resistenza;
+    document.getElementById("risultatoA").innerText = "Intesita: " + intensita + " A";
+
+    if (resistenza === 0 || tensione === 0)
+        {
+            alert("Inserisci dei valori validi.");
+            return;
+        }
+}
+
+function calcolaResistenza()
+{
+    const tensione = parseFloat(document.getElementById("tensioneC").value)
+    const intesita = parseFloat(document.getElementById("intensitaC").value)
+
+    const resistenza = tensione / intesita;
+    document.getElementById("risultatoC").innerText = "Intesita: " + resistenza + " A";
+
+    if (tensione === 0 || intesita === 0)
+        {
+            alert("Inserisci dei valori validi.");
+            return;
+        }
+}
